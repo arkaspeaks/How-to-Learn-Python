28261e3f48887d529e21e6cf37231c2841785cbf
Arka Banerjee <arkabanerjee381@gmail.com>
2024-06-18 05:51:50 +0000

    FUNCTIONS

---
+++ FUNCTIONS/02.py
@@ -0,0 +1,12 @@
+#the second step
+
+#THE SUM FUNCTION
+''' this function as the name suggests is used to find the sum of numbers'''
+
+list1 = (1,2,3,4)
+print (sum (list1))
+#this prints out the sum of the digits of list1 
+
+
+
+
