f70975d6d574d5c7b3c0f8e66834c4dadffe89de
Arka Banerjee <arkabanerjee381@gmail.com>
2024-06-18 05:27:48 +0000

    print function

---
+++ FUNCTIONS/01.py
@@ -0,0 +1,7 @@
+#this is the first step of learning python
+
+#THE PRINT STATEMENT 
+'''this function is used to print any desired output''' 
+
+print ("hello world")
+'''the above statement prints out the desired output of hello world'''
