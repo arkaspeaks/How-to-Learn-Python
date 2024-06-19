1bf38ef83b2b75f14bf504d4025f3f7c663b53da
Arka Banerjee <arkabanerjee381@gmail.com>
2024-06-19 18:30:07 +0000

    FUNCTIONS

---
+++ FUNCTIONS/04.py
@@ -0,0 +1,7 @@
+# a code which takes an input & counts the number of alphabets and give that output
+""" you still cant solve this """
+# well now you can 
+name = (input("sir your name?:"))
+print ("ok sir your name is", name )
+y= len(name)
+print ("i guess the number of characters in your name is ", y)
