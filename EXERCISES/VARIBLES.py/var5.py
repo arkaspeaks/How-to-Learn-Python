15af8dc825ec8d011dbd45ee1d46cb6edcd5e145
Arka Banerjee <arkabanerjee381@gmail.com>
2024-06-20 15:01:38 +0000

    EXERCISE

---
+++ EXERCISES/VARIBLES.py/var5.py
@@ -0,0 +1,8 @@
+'''W3 schools Python Exercises'''
+'''TOPIC : PYTHON VARIABLES'''
+
+#Insert the correct syntax to assign values to
+# multiple variables in one line and print it :
+
+x, y, z = "Orange", "Banana", "Cherry"
+print ( x,y,z)
