899235f95c23b2f01d20cdc25dd425dd102b2aa6
Arka Banerjee <arkabanerjee381@gmail.com>
2024-06-20 15:09:26 +0000

    EXERCISE

---
+++ EXERCISES/VARIBLES.py/var7.py
@@ -0,0 +1,8 @@
+'''W3 SCHOOLS PYTHON EXERCISES'''
+'''TOPIC: PYTHON VARIABLES '''
+
+#Insert the correct keyword to make the variable x belong to the global scope.
+
+def myfunc():
+ global x
+ x = "fantastic"
