49b85381701c51969b1693faa9a997473356583e
Arka Banerjee <arkabanerjee381@gmail.com>
2024-06-20 05:52:53 +0000

    EXERCISES

---
+++ EXERCISES/04.py
@@ -0,0 +1,10 @@
+'''W3 SCHOOLS PYTHON EXERCISES'''
+'''TOPIC: PYTHON COMMENTS'''
+
+# Use a multiline string to make the a multiline comment:
+
+'''
+This is a comment
+written in 
+more than just one line
+'''
