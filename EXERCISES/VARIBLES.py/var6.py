e811738aebfb24a43fd2755d6dfb967f9fba4b3e
Arka Banerjee <arkabanerjee381@gmail.com>
2024-06-20 15:04:17 +0000

    EXERCISE

---
+++ EXERCISES/VARIBLES.py/var6.py
@@ -0,0 +1,10 @@
+'''W3 schools Python Exercises'''
+'''TOPIC : PYTHON VARIABLES'''
+
+# Insert the correct syntax to assign the same value to all
+# three variables in one code line and print each one of them :
+
+x = y = z = "Orange"
+print (x)
+print (y)
+print (z)
