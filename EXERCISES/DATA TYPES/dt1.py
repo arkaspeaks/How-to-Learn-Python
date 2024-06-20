61d79aa67a054ac4c4f9bf9b60cfb45e01978518
Arka Banerjee <arkabanerjee381@gmail.com>
2024-06-20 15:28:10 +0000

    EXERCISE

--- EXERCISES/DATA TYPES/dt1.py
+++ EXERCISES/DATA TYPES/dt1.py
@@ -1,6 +1,14 @@
 '''W3 schools Python Exercises'''
 '''TOPIC: PYTHON DATATYPES'''
 
-#write a code which takes an input and prints the data type
-x = input()
-print (type(x))
+'''
+
+there are a few data types in Python:
+the most common ones are the-
+<str> (example: "hello world")
+<int> (example: 123)
+<float> (example: 123.23)
+<bool> (example: True or False)
+<NoneType> (example: None)
+
+'''
