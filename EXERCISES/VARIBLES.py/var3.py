ac452229e693b02d742f80a51277255e40f08171
Arka Banerjee <arkabanerjee381@gmail.com>
2024-06-20 06:44:12 +0000

    EXERCISE

---
+++ EXERCISES/VARIBLES.py/var3.py
@@ -0,0 +1,11 @@
+'''W3 schools Python Exercises'''
+'''TOPIC : PYTHON VARIABLES'''
+
+
+#Display the sum of 5 + 10, using two variables: x and y.
+#and print it
+
+x = 5
+y = 10 
+print (x+y)
+print (x, "+", y, "=", x+y)
