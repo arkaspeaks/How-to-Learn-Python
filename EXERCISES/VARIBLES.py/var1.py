4f4bea4b903a248767b6b06e57c385be193c8cd0
Arka Banerjee <arkabanerjee381@gmail.com>
2024-06-20 06:07:02 +0000

    Exercises

---
+++ EXERCISES/VARIBLES.py/var1.py
@@ -0,0 +1,8 @@
+'''W3 SCHOOLS PYTHON EXERCISES'''
+'''TOPIC: PYTHON VARIABLES '''
+
+#Create a variable named 'carname' and assign the value 'Volvo' to it.
+# & print it 
+
+carname = "Volvo"
+print (carname)
